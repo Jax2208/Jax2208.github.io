13648dc9d0892d775ce72ed61c70c7b52cf9fe77
Jax2208 <JackMc2208@gmail.com>
2021-09-29 11:39:58 +1000

    Update calculator.vue

--- components/calculator.vue
+++ components/calculator.vue
@@ -5,6 +5,6 @@
   <div class='justify-content-center text-center text-white'>
     <h1 class="text-white">Calculator</h1>
     <hr/>
-    <h1>Coming Soon<span class="badge badge-info">Coming</span></h1>
+    <h1><span class="badge badge-warning">COMING SOON</span></h1>
   </div>
 </template>
